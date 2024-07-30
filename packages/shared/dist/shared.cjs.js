5074a5fb0881b6ce8b2f52ec410f4ad1298f3dc5
kangzy <351658471@qq.com>
2024-07-30 15:53:54 +0800

    提交

--- packages/shared/dist/shared.cjs.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const isObject = (value) => typeof value === 'object' && value != null;
-const extend = Object.assign;
-
-exports.extend = extend;
-exports.isObject = isObject;
-//# sourceMappingURL=shared.cjs.js.map
